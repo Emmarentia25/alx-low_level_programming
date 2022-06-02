e752af9669400fe74979badc4ff1604b05a8d3b9
Emmarentia25 <steytler25@gmail.com>
2022-06-02 06:08:07 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints a text
- *   
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
