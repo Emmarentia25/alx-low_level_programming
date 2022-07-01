1c4a7198de38056c796126f3ed9b4aae0e4ca5d3
Emmarentia25 <steytler25@gmail.com>
2022-06-30 20:21:04 -0700

    task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,7 +29,7 @@ void _is_zero(char *argv[])
 
 	if (isn1 == 1 || isn2 == 1)
 	{
-		printf("0\n");
+		_putchar("0\n");
 		exit(0);
 	}
 }
@@ -66,7 +66,7 @@ int _checknum(char *argv[], int n)
 	for (ln = 0; argv[n][ln]; ln++)
 		if (!isdigit(argv[n][ln]))
 		{
-			printf("Error\n");
+			_putchar("Error\n");
 			exit(98);
 		}
 
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
+		_putchar("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
