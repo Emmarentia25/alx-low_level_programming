d1b369c9a0b4a67cf9a4f2d9c69616ecc5684d90
Emmarentia25 <steytler25@gmail.com>
2022-07-19 12:23:48 -0700

    task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,13 +2,14 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node
- * at a given position
- * @head: head of a list
+ * at a given position.
+ * @head: head of a list.
  * @idx: index of the list where the new node is
- * added
+ * added.
+ * @n: integer element.
  *
  * Return: the address of the new node, or NULL if it
- * failed
+ * failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
